94bd2dc7b119b96b50b5f588eca62e1d83c0d197
Senada Kadric <skadric@fordham.edu>
2020-04-23 00:32:09 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,28 @@
 # Write your methods here
+
+def loop_message_five_times(message_string)
+  count = 0
+  while count < 5
+    puts message_string
+    count += 1
+  end
+end
+
+def loop_message_n_times(message_string, integer)
+  count = 0
+  while count < integer
+    puts message_string
+    count += 1
+  end
+end
+
+def output_array(array)
+  #array.each { |ele| puts ele }
+end
+
+def return_string_array(array)
+  #new_array = []
+  #array.each do |ele|
+    #new_string = ele.to_s
+    #new_array << new_string
+end
